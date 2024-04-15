6e1879e75055b6ed7c0a5dfbb22504b830d6e27a
Ramy ElEssawy <ramy_essawy@yahoo.com>
2024-04-15 15:40:32 +0200

    Switch to Postgres

--- src/test/java/com/packt/mastering/openshift/example/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.packt.mastering.openshift.example;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
